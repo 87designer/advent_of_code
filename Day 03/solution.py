540cdab6bcbfc81366f1ac19021f08e1954cb3f6
Charles George <cgmac@macbook-pro.myfiosgateway.com>
2022-12-03 14:13:10 -0500

    Updated code for part 2 of puzzle

--- Day 03/solution.py
+++ Day 03/solution.py
@@ -2,11 +2,12 @@
 
 import os
 import sys
+import re
 import string
 from typing import Tuple
 
 
-def split_string(s:str) -> Tuple[str, str]:
+def split_string(s: str) -> Tuple[str, str]:
     """Takes a string with an even amount of characters and splits it into 2 strings.
 
         Parameters
@@ -25,7 +26,7 @@ def split_string(s:str) -> Tuple[str, str]:
     return string1, string2
 
 
-def compare_compartments(input_filepath: str) -> int:
+def compare_compartments(input_filepath: str) -> Tuple[int, int]:
     """Takes the filepath of a text file, reads text file parses
     rucksack item contents from 2 different pouches and identifies
     which item is present in each. Then prioritizes that item for
@@ -40,6 +41,8 @@ def compare_compartments(input_filepath: str) -> int:
     -------
     int
         sum of the Item Type Priorities
+    int
+        sum of the Badge Type Priorities
     """
     # Establish Item Type Priority
     items = list(string.ascii_letters)
@@ -51,9 +54,10 @@ def compare_compartments(input_filepath: str) -> int:
     with open(os.path.join(sys.path[0], input_filepath), "r") as f:
         input_file = f.read()
     rucksacks = input_file.split("\n")
+    groups = [x.strip().split('\n') for x in re.findall('((?:[^\n]+\n?){1,3})', input_file)]
 
     # Identify Item Rearrangement Priority
-    rearrangement_priority = {}
+    item_rearrangement_priority = {}
     for rucksack in rucksacks:
         compartment1, compartment2 = split_string(rucksack)
         compartment1_lower = list(filter(lambda c: c.islower(), compartment1))
@@ -61,20 +65,47 @@ def compare_compartments(input_filepath: str) -> int:
         item_in_both_compartments = set(compartment1_lower) & set(compartment2_lower)
         lower_item = list(item_in_both_compartments)
         if lower_item:
-            rearrangement_priority[rucksack] = {lower_item[0]: item_type_priority.get(lower_item[0])}
+            item_rearrangement_priority[rucksack] = {lower_item[0]: item_type_priority.get(lower_item[0])}
 
         compartment1_upper = list(filter(lambda c: c.isupper(), compartment1))
         compartment2_upper = list(filter(lambda c: c.isupper(), compartment2))
         item_in_both_compartments = set(compartment1_upper) & set(compartment2_upper)
         upper_item = list(item_in_both_compartments)
         if upper_item:
-            rearrangement_priority[rucksack] = {upper_item[0]: item_type_priority.get(upper_item[0])}
+            item_rearrangement_priority[rucksack] = {upper_item[0]: item_type_priority.get(upper_item[0])}
+
+    # Identify Badge Rearrangement Priority
+    badge_rearrangement_priority = {}
+    for group in groups:
+        pack1_lower = list(filter(lambda c: c.islower(), group[0]))
+        pack2_lower = list(filter(lambda c: c.islower(), group[1]))
+        pack3_lower = list(filter(lambda c: c.islower(), group[2]))
+        item_in_all_packs = set(pack1_lower) & set(pack2_lower) & set(pack3_lower)
+        lower_badge = list(item_in_all_packs)
+        if lower_badge:
+            badge_rearrangement_priority[tuple(group)] = {lower_badge[0]: item_type_priority.get(lower_badge[0])}
+
+        pack1_upper = list(filter(lambda c: c.isupper(), group[0]))
+        pack2_upper = list(filter(lambda c: c.isupper(), group[1]))
+        pack3_upper = list(filter(lambda c: c.isupper(), group[2]))
+        item_in_all_packs = set(pack1_upper) & set(pack2_upper) & set(pack3_upper)
+        upper_badge = list(item_in_all_packs)
+        if upper_badge:
+            badge_rearrangement_priority[tuple(group)] = {upper_badge[0]: item_type_priority.get(upper_badge[0])}
+
 
     # Identify Sum of Item Type Priorities
     item_type_priorities = []
-    for value in rearrangement_priority.values():
+    for value in item_rearrangement_priority.values():
         for v in value.values():
             item_type_priorities.append(v)
     itp_sum = sum(item_type_priorities)
 
-    return itp_sum
+    # Identify Sum of Item Type Priorities
+    badge_type_priorities = []
+    for value in badge_rearrangement_priority.values():
+        for v in value.values():
+            badge_type_priorities.append(v)
+    btp_sum = sum(badge_type_priorities)
+
+    return itp_sum, btp_sum
