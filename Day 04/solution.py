267ccd02bd0b384653b0cce30092877b28cbadcf
Charles George <cgdesigned@gmail.com>
2022-12-04 12:04:57 -0500

    Add README and Solution for Day 4 Puzzle Parts 1&2.

---
+++ Day 04/solution.py
@@ -0,0 +1,53 @@
+# Day 4: Camp Cleanup
+
+import os
+import sys
+from typing import Tuple
+
+
+def assignment_overlap_detector(input_filepath: str) -> Tuple[int, int]:
+    """Takes the filepath of a text file, reads text file, parses pairs
+    of section clean-up ranges, then returns the count of pairs where
+    one range is completely contained in the other, followed by the count
+    of pairs where there is any overlap at all.
+
+
+    Parameters
+    ----------
+    input_filepath : str
+        filepath to input txt file
+
+    Returns
+    -------
+    int
+        count of fully contained ranges
+    int
+        count of overlapping ranges
+    """
+    # Parse Text File Identifying Section Clean-up Pairs
+    with open(os.path.join(sys.path[0], input_filepath), "r") as f:
+        input_file = f.read()
+
+    pairs = [pair.split(",") for pair in input_file.split("\n")]
+    full_range_pairs = []
+    for pair in pairs:
+        int_ranges = []
+        for range_str in pair:
+            start_stop_values = range_str.split("-")
+            int_range = [i for i in range(int(start_stop_values[0]), int(start_stop_values[1]) + 1)]
+            int_ranges.append(int_range)
+        full_range_pairs.append(int_ranges)
+
+    fully_contained_ranges = 0
+    overlapping_ranges = 0
+
+    for range_a, range_b in full_range_pairs:
+        if range_a is range_b:
+            fully_contained_ranges += 1
+        else:
+            if set(range_a).issubset(range_b) | set(range_b).issubset(range_a):
+                fully_contained_ranges += 1
+        if len(list(set(range_a) & set(range_b))) > 0:
+            overlapping_ranges += 1
+
+    return fully_contained_ranges, overlapping_ranges
