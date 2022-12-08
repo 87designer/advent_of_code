14950dc349c6a59fdc12fcaa0e6bb9a45addbea5
Charles George <cgdesigned@gmail.com>
2022-12-07 23:56:17 -0500

    Update README

--- Day 07/solution.py
+++ Day 07/solution.py
@@ -1,13 +1,70 @@
-# Day 7:
+# Day 7: No Space Left On Device
 
-import re
 import input_load as load
+from typing import Tuple
 
 
-def main(input_filepath: str) -> None:
-    # Parse Text File Identifying Supply Crate Stack Drawing & Rearrangement Procedures List
+def main(input_filepath: str) -> Tuple[int, int]:
+    """Takes the filepath of a text file and reads lines from
+    terminal output. Parses terminal commands and sums directories
+    with a total size of at most 100000 as well as identifies size
+    of smallest viable folder for deletion.
+
+    Parameters
+    ----------
+    input_filepath : str
+        filepath to input txt file
+
+    Returns
+    -------
+    int
+        sum size of folders <= 100000
+    int
+        size of smallest viable folder for deletion
+    """
+    folders = {}
+    filepath = []
+
+    # Parse Text File Identifying Terminal Output Lines
     input_file = load.txt_to_str(input_filepath)
+    lines = [line for line in input_file.strip().split('\n')]
+    for line in lines:
+        line_data = line.strip().split()
+        if line_data[1] == 'cd':
+            if line_data[2] == '..':
+                filepath.pop()
+            else:
+                filepath.append(line_data[2])
+        elif line_data[1] == 'ls':
+            continue
+        elif line_data[0] == 'dir':
+            continue
+        else:
+            folder_size = int(line_data[0])
+            for i in range(1, len(filepath)+1):
+                path = '/'.join(filepath[:i])
+                if path in folders.keys():
+                    size_count = {path: (folders.get(path) + folder_size)}
+                    folders.update(size_count)
+                else:
+                    folders.setdefault(path, folder_size)
 
-    return None
+    # Disk Space
+    total = 70000000
+    needed = 30000000
+    used = folders.get('/')
+    used_target = total-needed
 
-main('test_input.txt')
+    # Puzzle Answers
+    total_size = 0
+    viable_folders = []
+
+    for folder, size in folders.items():
+        if size <= 100000:
+            total_size += size
+        if (used-size) <= used_target:
+            viable_folders.append(size)
+
+    smallest_viable = sorted(viable_folders)[0]
+
+    return total_size, smallest_viable
--- Day 07/solutionb.py
+++ Day 07/solutionb.py
@@ -1,121 +1,40 @@
 # Day 7:
 
-import re
+from collections import defaultdict
 import input_load as load
-from typing import Tuple
 
+input_file = load.txt_to_str('puzzle_input.txt')
+data = input_file.strip()
+lines = [x for x in data.split('\n')]
 
-def main(input_filepath: str) -> None:
-    # Parse Text File Identifying Supply Crate Stack Drawing & Rearrangement Procedures List
-    input_file = load.txt_to_str(input_filepath)
+SZ = defaultdict(int)
+filepath = []
+for line in lines:
+    words = line.strip().split()
+    if words[1] == 'cd':
+        if words[2] == '..':
+            filepath.pop()
+        else:
+            filepath.append(words[2])
+    elif words[1] == 'ls':
+        continue
+    elif words[0] == 'dir':
+        continue
+    else:
+        sz = int(words[0])
+        for i in range(1, len(filepath)+1):
+            SZ['/'.join(filepath[:i])] += sz
 
-    # print(input_file)
-    # print()
+total_ds = 70000000
+used_ds = SZ['/']
+ds_needed = 30000000
+ds_to_free = used_ds - (total_ds - ds_needed)
 
-    class Dir:
-        def __init__(self, path, name):
-            self.path = path
-            self.name = name
-            self.contents = []
-            self.size = 0
-
-
-    dirs = {}
-    dir_names = ['/']
-    dir_contents = {}
-
-    # Split into command blocks
-    delimiter = '$'
-    command_groups = [delimiter+l[:-1] for l in input_file.split(delimiter) if l]
-
-    cwd = '/'
-    # Establish list of file paths
-    for group in command_groups:
-        group = group.split('\n')
-        # print(group)
-        if group[0][2:4] == 'cd':
-            # print(cwd)
-            split_group = group[0].split(' ')
-            if cwd == split_group[-1]:
-                pass
-            else:
-                if cwd == '/':
-                    cwd = cwd + split_group[-1]
-                    # print('Change Dir command...')
-                    # print(cwd)
-                    if cwd in dir_names:
-                        pass
-                    else:
-                        dir_names.append(cwd)
-                elif split_group[-1] == '..':
-                    if len(cwd[:-2]) > 1:
-                        cwd = cwd[:-2]
-                        if cwd in dir_names:
-                            pass
-                        else:
-                            dir_names.append(cwd)
-                        # print('exit Dir command...')
-                        # print(cwd)
-                    else:
-                        cwd = '/'
-                        # print('exit Dir command...')
-                        # print(cwd)
-                else:
-                    cwd = cwd+'/'+split_group[-1]
-                    # print('Change Dir command...')
-                    # print(cwd)
-                    if cwd in dir_names:
-                        pass
-                    else:
-                        dir_names.append(cwd)
-                    # Dir()
-        if group[0][2:4] == 'ls':
-            dir_contents[cwd] = group[1:]
-            # print('List command...')
-        # print()
-
-    print("FilePaths:", dir_names)
-    print()
-
-
-    # Create Directory Objects from path list
-    for d in dir_names:
-        dirs[d] = Dir(name=d[-1], path=d)
-        dirs[d].contents = dir_contents.get(d)
-
-    print()
-
-    ans = 0
-    sub_count = -1
-    for k in sorted(dirs.keys(), key=len, reverse=True):
-        folder_size = 0
-        sub_count += 1
-        print(k)
-        for c in dirs[k].contents:
-            file_size = c.split(' ')[0]
-            file_name = '/'+c.split(' ')[1]
-            if file_size == 'dir':
-                # print(file_name, file_size)
-                dirs[k[:2]].size += dirs[k].size
-                folder_size += dirs[k].size
-            else:
-                # print(file_name, file_size)
-                dirs[k].size += int(file_size)
-                folder_size += int(file_size)
-                if len(k) > 2:
-                    # parent = k[:len(k) // 2]
-                    # dirs[parent].size += int(file_size)
-                    folder_size += int(file_size)
-        # print()
-        # print(sub_count)
-        print(folder_size)
-        if folder_size <= 100000:
-            ans += folder_size
-        # print('Root Size: ', (ans - dirs.get('/').size - dirs.get(sorted(dir_names, key=len, reverse=True)[0]).size*(sub_count)))
-
-    for k in sorted(dirs.keys(), key=len, reverse=True):
-        print(f'{k} file size: {dirs[k].size}')
-    print()
-    return print(ans)
-
-main('test_input.txt')
+ans1 = 0
+ans2 = 1e9
+for k,v in SZ.items():
+    if v <= 100000:
+        ans1 += v
+    if v >= ds_to_free:
+        ans2 = min(ans2, v)
+print(ans1, ans2)
